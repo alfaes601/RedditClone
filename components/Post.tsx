53321a2472a991f731e38b58319919aede66cda7
Esau Alvarez <alfaes601@gmail.com>
2022-08-11 17:30:19 -0500

    Comments, SubredditRow, querys, mutations, dynamic routes

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,5 @@
 import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "./Avatar";
 import TimeAgo from "react-timeago";
 import {
@@ -10,19 +10,101 @@ import {
   DotsHorizontalIcon,
 } from "@heroicons/react/outline";
 import Link from "next/link";
+import { Jelly } from "@uiball/loaders";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
+import { useMutation, useQuery } from "@apollo/client";
+import { GET_VOTES_BY_POST_ID } from "../graphql/querys";
+import { ADD_VOTE } from "../graphql/mutations";
 
 type Props = {
   post: Post;
 };
 function Post({ post }: Props) {
+  const [vote, setVote] = useState<boolean>();
+  const { data: session } = useSession();
+
+  const { data, loading, error } = useQuery(GET_VOTES_BY_POST_ID, {
+    variables: {
+      post_id: post?.id,
+    },
+  });
+
+  const [addVote] = useMutation(ADD_VOTE, {
+    refetchQueries: [GET_VOTES_BY_POST_ID, "getVotesByPostId"],
+  });
+
+  const upVote = async (isUpvote: boolean) => {
+    if (!session) {
+      toast("You need to sign in to vote");
+      return;
+    }
+
+    if (vote && isUpvote) return;
+    if (vote === false && !isUpvote) return;
+
+    await addVote({
+      variables: {
+        post_id: post.id,
+        username: session?.user?.name,
+        upvote: isUpvote,
+      },
+    });
+  };
+
+  useEffect(() => {
+    const votes: Vote[] = data?.getVotesByPostId;
+
+    //latest vote (as we sorted by newely created first in SQL query)
+    const vote = votes?.find(
+      (vote) => vote.username == session?.user?.name
+    )?.upvote;
+    setVote(vote);
+  }, [data]);
+
+  const displayVotes = (data: any) => {
+    const votes: Vote[] = data?.getVotesByPostId;
+    const displayNumber = votes?.reduce(
+      (total, vote) => (vote.upvote ? (total += 1) : (total -= 1)),
+      0
+    );
+
+    if (votes?.length === 0) return 0;
+
+    if (displayNumber === 0) return votes[0]?.upvote ? 1 : -1;
+
+    return displayNumber;
+  };
+
+  if (!post)
+    return (
+      <div className="flex w-full items-center justify-center p-10 text-xl">
+        <Jelly size={50} color="#ff4501" />
+      </div>
+    );
+
   return (
     <Link href={`/post/${post.id}`}>
       <div className="flex cursor-pointer rounded-md border border-gray-300 bg-white shadow-sm hover:border-gray-600">
         {/* votes */}
         <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
-          <ArrowUpIcon className="voteButtons hover:text-red-400" />
-          <p className="text-xs font-bold text-black">0</p>
-          <ArrowDownIcon className="voteButtons hover:text-red-400" />
+          <ArrowUpIcon
+            onClick={() => {
+              upVote(true);
+            }}
+            className={`voteButtons hover:text-red-400 ${
+              vote && "text-red-400"
+            }`}
+          />
+          <p className="text-xs font-bold text-black">{displayVotes(data)}</p>
+          <ArrowDownIcon
+            onClick={() => {
+              upVote(false);
+            }}
+            className={`voteButtons hover:text-blue-400 ${
+              vote === false && "text-blue-400"
+            }`}
+          />
         </div>
         <div className="p-3 pb-1">
           {/* header */}
--- graphql/querys.ts
+++ graphql/querys.ts
@@ -32,9 +32,43 @@ export const GET_ALL_POST = gql`
     }
   }
 `;
+
 export const GET_ALL_POST_BY_TOPIC = gql`
   query MyQuery($topic: String!) {
     getPostListByTopic(topic: $topic) {
+      body
+      created_at
+      id
+      image
+      title
+      subreddit_id
+      username
+      comments {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      subreddit {
+        created_at
+        id
+        topic
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`;
+
+export const GET_POST_BY_POST_ID = gql`
+  query MyQuery($post_id: ID!) {
+    getPostByPostId(post_id: $post_id) {
       body
       created_at
       id
@@ -71,6 +105,27 @@ export const GET_SUBREDDIT_BY_TOPIC = gql`
       id
       topic
       created_at
+    }
+  }
+`;
+export const GET_SUBREDDIT_WITH_LIMIT = gql`
+  query MyQuery($limit: Int!) {
+    getSubredditListLimit(limit: $limit) {
+      id
+      topic
+      created_at
+    }
+  }
+`;
+
+export const GET_VOTES_BY_POST_ID = gql`
+  query MyQuery($post_id: ID!) {
+    getVotesByPostId(post_id: $post_id) {
+      id
+      created_at
+      post_id
+      upvote
+      username
     }
   }
 `;
